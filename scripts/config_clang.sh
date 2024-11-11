7c5c41038a0a85bcaa684aafd062e8ee8d08d01f
sebawoz02 <268491@student.pwr.edu.pl>
2024-11-11 15:34:56 +0100

    [CI] removed unnecessary fragments

--- scripts/config_clang.sh
+++ scripts/config_clang.sh
@@ -8,7 +8,4 @@ sudo apt-get -y install clang clangd clang-format clang-tidy clang-tools lld
 # Install cmake just in case.
 sudo apt-get -y install cmake
 
-# GLFW dependencies.
-sudo apt-get install -y libxinerama-dev libxcursor-dev libxi-dev libxrandr-dev libgl1-mesa-dev
-
 echo "Installation complete."
--- scripts/config_gcc.sh
+++ scripts/config_gcc.sh
@@ -4,7 +4,4 @@ sudo apt-get update
 
 sudo apt-get install -y cmake
 
-# GLFW dependencies.
-sudo apt-get install -y libxinerama-dev libxcursor-dev libxi-dev libxrandr-dev libgl1-mesa-dev
-
 echo "Installation complete."
